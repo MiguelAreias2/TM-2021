5f987d1f6336f090f45fbb133fcb6840047cfafb
MiguelAreias2 <79462661+MiguelAreias2@users.noreply.github.com>
2021-03-01 15:25:55 +0000

    exercicio2-MiguelAreias

---
+++ Aula 2/exercicio2/Template/script.js
@@ -0,0 +1,22 @@
+// Your JS Script her
+let addedNumbers = [];
+
+function pushArray() {
+    const number = document.getElementById("numbers").value
+    addedNumbers.push(number)
+    document.getElementById("arrayNumbers").innerText = addedNumbers.toString()
+}
+
+function calculateMaxNumber() {
+    if (addedNumbers.length >= 5) {
+        const maxNumber = addedNumbers.reduce((a, b) => Math.max(a, b));
+
+        const maxNumberCopy = addedNumbers.reduce(function (a, b) {
+            return Math.max(a, b);
+        });
+
+        const maxNumberCopy2 = Math.max.apply(Math, addedNumbers);
+
+        document.getElementById("maxNumber").innerText = maxNumber + " é igual a " + maxNumberCopy + " é igual a " + maxNumberCopy2
+    }
+}
